432a2ea373e28c924cd83f45bf98dc750055d7b8
yuzuha6761 <yuzuha6761@gmail.com>
2026-04-17 11:06:38 +0800

    update: hide useless features

--- web/src/components/table/model-pricing/layout/header/SearchActions.jsx
+++ web/src/components/table/model-pricing/layout/header/SearchActions.jsx
@@ -118,10 +118,10 @@ const SearchActions = memo(
             )}
 
             {/* 显示倍率开关 */}
-            <div className='flex items-center gap-2'>
+            {/* <div className='flex items-center gap-2'>
               <span className='text-sm text-gray-600'>{t('倍率')}</span>
               <Switch checked={showRatio} onChange={setShowRatio} />
-            </div>
+            </div> */}
 
             {/* 视图模式切换按钮 */}
             <Button
